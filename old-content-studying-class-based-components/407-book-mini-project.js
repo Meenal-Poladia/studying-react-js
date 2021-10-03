f31828580854586adcf72bba6c6bd8e892930c17
Meenal-Poladia <meenal.poladia94@gmail.com>
2021-10-03 09:59:22 +0530

    407-book-mini-project.js

---
+++ old-content-studying-class-based-components/407-book-mini-project.js
@@ -0,0 +1,34 @@
+/*
+Book mini project
+
+    React was developed by facebook in the year 2011 and is currently the most popular
+    Javascript library used to build user interfaces.
+
+    Some Competitors of react are Angular, Svelte and Vue.
+
+    When it comes to react, it's all about components and you can think of components as
+    independent chunks of user interfaces. Components can be as small as one element.
+    Component = User Interface
+
+    The benefit of the component is that you can build a bunch of independent, isolated and most
+    importantly, reusable user interfaces that you can then piece it together just like Lego blocks and
+    as a result build even super complex apps without going insane.
+
+    While their app can have as many components as you would like, it will always, always have at least
+    one called root component.
+
+    The benefits of react:
+    1. You can build independent pieces of user interfaces, meaning changing logic or layout
+       in one command will not break your whole up.
+    2. Once the component is ready to go. You can reuse it all throughout Europe, but component
+       code is still stored in one place. So if you ever need to make some changes, you don't have
+       to run around like a headless chicken, simply locate the component, apply the changes and
+       all the instances will be automatically updated.
+    3. Behind the scenes react is using something called virtual dom, where only the component that
+       needs to be updated is effective. So, the speed is also fast which is done without rendering
+       the whole which in turn, of course, increases the speed of your final product and as a result,
+       the user experience as well.
+    4. I can reuse the component all throughout my application so if you want to make some changes
+       then the changes need to be made only in one place and then all the instances will be
+       automatically updated.
+ */
