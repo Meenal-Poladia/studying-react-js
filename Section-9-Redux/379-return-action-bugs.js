0f69d88e0cfff6cb6b852fd28579c80d3b7381f1
Meenal-Poladia <meenal.poladia94@gmail.com>
2021-10-11 15:28:11 +0530

    Adding 379-return-action-bugs.js

---
+++ Section-9-Redux/379-return-action-bugs.js
@@ -0,0 +1,32 @@
+/*
+    Return and Action Bugs
+
+    1. We can return anything we want from the reducer but we have passed a number as
+    a key in the initial state and later if we set it as a string then we get an
+    error. So, if the action.type does not match then we should always return the
+    old state.
+
+    2. If our action.type passed in the store.dispatch does not match the action.type in the
+    reducer function then then will be no error message and the old state will be returned if
+    we have passed to return the old state.
+
+
+example 1:
+example 1:
+const initialStore = {
+    count: 0;
+}
+
+//reducer function
+function reducer (state, action) {
+    console.log({state, action})
+    if(action.type === "DECREASE") {
+        return {count: state.count - 1}           //Correct. we are creating a new state. Creating a new object.
+    }
+    return state
+}
+const store = createStore(reducer, initialStore);
+store.dispatch({ type: "DECREASE" });
+store.dispatch({ type: "INCREASE" });        // We have not defined it but it is still dispatched successfully.
+
+ */
