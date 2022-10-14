2b525c6c026b3748efaca86b0f0f156ab71cd7df
Sajeel-Bongale <sajeelbongale@gmail.com>
2022-10-14 10:41:46 +0530

    002.react-code-is-written-in-a-declarative-way.js

---
+++ 003.react-basics-and-working-with-components/002.react-code-is-written-in-a-declarative-way.js
@@ -0,0 +1,22 @@
+/*
+
+    React code is written in a Declarative way
+
+    In react we combine HTML, CSS and JavaScript in all these components and then we combine all of these components
+    together to build the entire user interface.
+
+    React is all about components and about building these components which we combined together.
+    React allows us to create these re-usable and reactive components.
+
+    React uses something which is called a declarative approach for building these components.
+    It basically means that with React, you will not tell React that a certain HTML element should be created and
+    inserted in a specific place on the user interface as you would be doing it using Vanilla JavaScript.
+    Instead with React and that's really important, you will always define the desired end state, the target state
+    or possibly also different target states depending on different conditions and it's then React's job to figure out
+    which elements on the actual webpage might need to be added or removed or updated.
+    And you don't write these concrete DOM updating instructions on your own as you would be doing it with just
+    JavaScript, instead with React and React components, you just define these end states and under which conditions
+    which state should be used and then React will do all of the rest under the hood.
+
+    It's a bit like magic, it's of course not actual magic but it will make your life as a developer much easier.
+ */
