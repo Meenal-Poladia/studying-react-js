2e3d8a407912d72fcf36c2d8b5c906435bb8aa27
Sajeel-Bongale <sajeelbongale@gmail.com>
2022-10-14 10:44:41 +0530

    003.creating-a-new-react-project.js

---
+++ 003.react-basics-and-working-with-components/003.creating-a-new-react-project.js
@@ -0,0 +1,24 @@
+/*
+    Creating a new react project
+
+    The easiest way of getting started with a React project is to use a tool called Create React App.
+
+    This is a tool which you can use to create React projects. Which are basically pre-configured folders
+    with some basic React code files and most importantly a bunch of configuration files which help you build
+    that React App for production use because it turns out that there will be a couple of transformation and
+    optimization steps between developing any React App and running a React App in production.
+
+    This project which is created with Create React App, will give you a nice development environment
+    with a development web server which allows you to preview the application locally on your machine.
+    It will allow you to preview this application in a way where the browser will automatically update
+    the page whenever you make changes in your code.
+
+    It simply simplifies our development process. It adds a couple of key transformation steps which we need
+    and it will later also help us optimize our React code before we push it to a production server.
+    We need node.js as react is a javascript library and the runtime for JavaScript is node so it allows us to run
+    JavaScript code outside of the browser.
+
+    The package.json file in the end holds all the dependencies of this project, for example, React, but also a couple
+    of development dependencies, which are involved with these transformation and optimization steps.
+    The SRC folder holds the actual source code you will be working on.
+ */
