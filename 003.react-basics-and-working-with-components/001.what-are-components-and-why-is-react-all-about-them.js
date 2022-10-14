e5f0ce63ac147cea77346417fe45cc12ff10d148
Sajeel-Bongale <sajeelbongale@gmail.com>
2022-10-14 10:38:38 +0530

    001.what-are-components-and-why-is-react-all-about-them.js

---
+++ 003.react-basics-and-working-with-components/001.what-are-components-and-why-is-react-all-about-them.js
@@ -0,0 +1,21 @@
+/*
+    What are components and why is react all about them
+
+    React is a javascript library for building user interfaces. React helps to build complex interfaces easily.
+    React is all about components.
+
+    They are reusable building blocks in user interface. They are a combination of HTML, CSS and Javascript code.
+    We combine various components to create an user interface.
+
+    React embraces this concept of components because of that reusability aspect and because it allows us to
+    separate our concerns. Having reusable building blocks helps us avoid repetition.
+
+    While having separation of concerns helps us with keeping our code base small and manageable instead of having one
+    large file which holds all the HTML code and all the JavaScript logic.
+
+    For the entire user interface we have small separated units, these components where every component
+    has one clear concern, one focus, one specific task it focuses us on.
+
+    The reusability logic react has is similar to that of functions where we create multiple functions and call
+    one function from another so that they can be reused and have separation of concerns.
+ */
