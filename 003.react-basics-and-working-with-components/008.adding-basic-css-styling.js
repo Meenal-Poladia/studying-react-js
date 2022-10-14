40ee19d4e44d7682edcf1837fadfc3cd0168d1c2
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-10-14 11:36:35 +0530

    008.adding-basic-css-styling.js

---
+++ 003.react-basics-and-working-with-components/008.adding-basic-css-styling.js
@@ -0,0 +1,15 @@
+/*
+
+    Adding basic css styling
+
+    Including css file in the javascript code makes the build process aware the file needs to be included.
+
+    Either a global style sheet needs to be considered or we can have one stylesheet per component which needs to be
+    imported into the component.
+
+    Here we do not add the keyword class but add className to add the css style class. And this can be strange, but you
+    have to keep in mind that this here is not really HTML. It looks like HTML, but it's this special
+    JSX syntax invented by the React team.
+    The class attribute was renamed to className because class is a reserved word in JavaScript.
+
+ */
