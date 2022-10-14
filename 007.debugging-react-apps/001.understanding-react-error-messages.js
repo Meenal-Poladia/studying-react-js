82b6598b589418a6ae7d0cb4e53023b12af88007
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-10-14 13:15:45 +0530

    Updated 001.understanding-react-error-messages.js

--- 007.debugging-react-apps/001.understanding-react-error-messages.js
+++ 007.debugging-react-apps/001.understanding-react-error-messages.js
@@ -16,4 +16,11 @@
     Never panic when you see an error message, read error messages, have a look at the line or the file it's coming from,
     have a look at the code snippet it might be showing you and then look at the problematic code part,
     at the problematic code snippet.
+
+    Another most frequent error is: Encountered two children with the same key. Keys should be unique so that
+    components maintain their identity across updates....
+    This warning is set when we are trying to render a list and the keys in the list are the same. This can also lead to
+    logical error wherein we are trying to delete an item with a particular item but delete another as the id for
+    others are the same.
+
  */
