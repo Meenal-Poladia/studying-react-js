a1a9bd127a8c320519e467404c69cd8cd2bb225b
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-10-14 11:25:45 +0530

    006.how-react-works.js

---
+++ 003.react-basics-and-working-with-components/006.how-react-works.js
@@ -0,0 +1,16 @@
+/*
+
+    How react works
+
+    With React we define the desired target state and React is then responsible for generating and running the actual
+    DOM instructions which update what's visible on the screen.
+
+    Javascript follows an imperative approach where we need to first create an element, later append or prepend it and
+    then sets its inner text to something so that it is visible on the DOM. However, in react we just add a code
+    looking like HTMl inside which we add the desired tag and the content which we want to render on the screen.
+
+    Thus, javascript follows an imperative approach because you are giving clear instructions, clear step-by-step
+    instructions, what JavaScript and the browser should be doing. However, this can become cumbersome if we are
+    creating large scale applications.
+
+ */
