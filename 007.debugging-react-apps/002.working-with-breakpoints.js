0b70ba6c64a1f72257a5e0584e4160dbf2ebe7c0
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-10-14 13:16:15 +0530

    002.working-with-breakpoints.js

---
+++ 007.debugging-react-apps/002.working-with-breakpoints.js
@@ -0,0 +1,23 @@
+/*
+
+    Working with breakpoints
+
+    Breakpoints can be used for finding out an error in our code snippet. For accessing the breakpoints we need to
+    access the sources tab of the developers console.
+    React development process, gives the browser a bridge, between the code that runs in the browser and the code we
+    wrote,and we can access this code in the sources tab, by going to that User's folder. If the files are not in the
+    users folder then we should also check the webpack folder.
+
+    To set the breakpoint we need to go to the file for which we want to see the error. By clicking on the line number
+    we set the breakpoint on the file. once we add the a break point and then execute the action that triggers that
+    part of the code where the break point is placed code execution stops.
+    The line is then highlighted in blue in the debugger once executed.
+
+    To step into the next function we click on the step into next function icon. This can be done to see the values
+    set by the previous function. We can also hover over the values to see the values.
+
+    To go to next function we can click o step over next function icon.
+
+    Once debugging is done we can click on the play icon which represents resuming script execution.
+
+ */
