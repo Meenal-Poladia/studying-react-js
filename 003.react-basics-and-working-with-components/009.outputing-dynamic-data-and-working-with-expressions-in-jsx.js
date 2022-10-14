d016d52e14c5b046c7bdd447d24d2c7a736a70b4
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-10-14 11:38:14 +0530

    009.outputing-dynamic-data-and-working-with-expressions-in-jsx.js

---
+++ 003.react-basics-and-working-with-components/009.outputing-dynamic-data-and-working-with-expressions-in-jsx.js
@@ -0,0 +1,16 @@
+/*
+
+    Outputting dynamic data and working with expressions in jsx
+
+    Hard coded values can be replaced by dynamic values using {} inside which javascript code can be outputted.
+
+    Single curly braces opening and closing. And the special thing about these curly braces
+    inside of your JSX code snippets is that in these curly braces between them, you can run basic JavaScript expressions.
+
+    JavaScript will extract the value stored in this constant and basically inject it here into this HTML code.
+
+
+example - {1 + 1}
+{Math.Random()}
+
+ */
