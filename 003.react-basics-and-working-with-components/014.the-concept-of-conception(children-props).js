1920c474f00ec7e0587480d2925f40e1f41fef60
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-10-18 11:27:51 +0530

    014.the-concept-of-conception(children-props).js

---
+++ 003.react-basics-and-working-with-components/014.the-concept-of-conception(children-props).js
@@ -0,0 +1,12 @@
+/*
+
+    The concept of conception (children-props)
+
+    The approach of building a user interface from smaller building blocks is called composition.
+
+    Inside the child component we have the props which we get from the parent component. The word children is reserved
+    for the props passed down from the parent component so the word children is a reserved word.
+
+    One attribute which every component receives even though a prop is not passed is called as props.
+
+ */
