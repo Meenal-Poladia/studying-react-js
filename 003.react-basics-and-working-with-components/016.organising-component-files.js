836c39f58bf87924eba57dd3f6ea2cae0710d4b2
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-10-18 11:34:41 +0530

    016.organising-component-files.js

---
+++ 003.react-basics-and-working-with-components/016.organising-component-files.js
@@ -0,0 +1,15 @@
+/*
+
+    Organising component files
+
+    As the project gets bigger and bigger there is a need to organise the file in folders and sub-folders.
+    The various folder which we can create in our project are:
+        1. UI components which are not tied up to any feature in particular.
+        2. Some files we are dealing with expenses and its various features which can be created in the Expenses folder
+           Thus, creating a context based folder. Expenses folder will be sibling of UI folder.
+        3. Creating a logical nesting of components which are component specific. It helps to understand the parent
+           child relationship better.
+
+    This helps to keep our components and the project organised.
+
+ */
