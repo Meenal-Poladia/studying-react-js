d6da1e64ff3a7329ced9cedce822886a00cd6b23
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-10-18 12:00:28 +0530

    003.using-react-devtools.js

---
+++ 007.debugging-react-apps/003.using-react-devtools.js
@@ -0,0 +1,22 @@
+/*
+
+    Using React devtools
+
+    React devtools is a dedicated tool which is available for React Development that helps us with React specific
+    problems ans also in understanding our React Application better.
+
+    React devtools are available for download on google for browsers like firefox however if we want to use it on
+    chrome then there is an extension for using react devtools. We can simply visit the extension store and download
+    the extension on our browser.
+    After adding the extension we will have 2 new tabs on our inspect window: Components and Profiler.
+
+    If we click on Components then we can see a component tree which we are building using react whereas the
+    Elements tabs on the inspect window shows the DOM which we can use for debugging the HTML code. As we hover
+    over the elements in the Components tab the UI side of it starts getting highlighted. To get more information
+    about the component we can click on them. We can get information about the props on them, the handlers attached to
+    them, the component rendering it and the hooks attached on the component. Changing anything on the dev tools is
+    also reflected on the UI.
+
+    We can also search for a particular element or change the settings of the tabs using the settings corg.
+
+ */
