d249435f42133095a6bdb9850ae5897dd93261c0
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-10-18 11:26:29 +0530

    011.splitting-components-into-multiple-components.js

---
+++ 003.react-basics-and-working-with-components/011.splitting-components-into-multiple-components.js
@@ -0,0 +1,17 @@
+/*
+
+    Splitting components into multiple components
+
+    As we work on our React application and on our React components, we will notice that in any project that our
+    components eventually become bigger and bigger.
+
+    React allows you to split your application into smaller building blocks, where every building block, every component
+    is focused on one core task and then we build our overall user interface, by combining these building blocks.
+
+    By doing that, we keep every component on its own relatively small and manageable, and we keep its code base small
+    and manageable and we still can build a complex user interface.
+
+    Components and React are just regular functions with the extra twist of returning this JSX code.
+    The bigger our application gets and our components become, the more we wanna look into splitting them up.
+
+ */
