37d0b86b79b985789449ab46798b5395d18c6126
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-10-19 12:10:27 +0530

    003.react-fragments.js

---
+++ 008.diving-deeper-working-with-fragements-portals-refs/003.react-fragments.js
@@ -0,0 +1,11 @@
+/*
+
+    React fragment
+
+    <React.Fragment> or <> </> is an alternative to Wrapper component which we created in order to avoid div soups.
+    To use <React.Fragment> we need to import Fragment from react library.
+
+    These two syntax's here render empty wrappers, which don't render any actual HTML element to the DOM. These two
+    build in syntax's behind the scene work like the wrapper component we just created.
+
+ */
