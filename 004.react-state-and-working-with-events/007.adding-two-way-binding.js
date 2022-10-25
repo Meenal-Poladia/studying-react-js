f1ba60229fd79065c9d86d38e64111a11469be5c
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-10-25 14:41:54 +0530

    007.adding-two-way-binding.js

---
+++ 004.react-state-and-working-with-events/007.adding-two-way-binding.js
@@ -0,0 +1,16 @@
+/*
+
+    Adding two way binding
+
+    Two-way binding, simply means that for inputs we don't just listen to changes, but we can also pass a new value
+    back into the input.
+    So that we can reset or change the input programmatically.
+
+    All we have to do is add the value attribute, which is a default attribute, to this input element.
+    This will set the internal value property, which every input element has and we can set it to a new value. We will
+    bind the value property to the useState.
+
+    Two-way binding is very useful when you're working with forms because it allows you to gather user input,
+    but then also change it.
+
+ */
