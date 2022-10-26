33767e8e44ee66a3e5d9df1f6f513b45f922679b
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-10-26 12:35:08 +0530

    010.controlled-vs-uncontrolled-components-and-stateless-vs-stateful-components.js

---
+++ 004.react-state-and-working-with-events/010.controlled-vs-uncontrolled-components-and-stateless-vs-stateful-components.js
@@ -0,0 +1,15 @@
+/*
+
+    Controlled vs uncontrolled components and stateless vs stateful components
+
+    Whenever we use two way binding we are controlling the component which means that the value which is used in the
+    component is passed on from the parent component through props. Thus, the real logic resides in the parent
+    component. The child just consumes it to render on the UI.
+
+    Stateful Component: In React, a stateful component is a component that holds some state.
+    Stateless Component: Stateless components, by contrast, have no state. They are also called as presentational or dumb
+    components.
+
+    Note that both types of components can use props.
+
+ */
