6147f93579d92b37e6cf769b8cd47bf3aec70fd8
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-11-03 10:50:45 +0530

    003.introducing-styled-components.js

---
+++ 006.styling-react-components/003.introducing-styled-components.js
@@ -0,0 +1,18 @@
+/*
+
+    Introducing styled components
+
+    When there are multiple developers working on a single project then it may happen that they have the same class
+    name which may result in wrong styling being applied or css bleeding which means that the css is not scoped.
+
+    One way to avoid this is by using styled components. This package can be searched online. Styled components is a
+    package that helps to build components that have certain styles attached to them where the style affects only the
+    component. To use this package first we need to install it.
+
+    To learn more about styled components refer to the link given below:
+    https://styled-components.com/
+
+    They styled components package assigns dynamic class to the element so that the classes remain scoped and styles
+    do not bleed in the project.
+
+ */
