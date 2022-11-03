bafc4a346f5bf3e16248375d14cf2eee66fb8918
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-11-03 10:17:33 +0530

    005.adding-conditional-return-statements.js

---
+++ 005.rendering-list-and-conditional-content/005.adding-conditional-return-statements.js
@@ -0,0 +1,15 @@
+/*
+
+    Adding Conditional Return Statements
+
+    If we want to add a style dynamically in react then we use the style attribute. The style attribute in react
+    takes an object. We use {{}} to apply styles in react. The first curly bracket is for outputting content dynamically
+    and the inner one is for sending an object to the style attribute.
+
+    If the css attribute has 2 or more words then we should either use camel case or - between both the words.
+
+    For example -
+    const braHeight = 3 + actualHeight;
+
+    <div style={{height: barHeight, "backgroundColor": "red", "background-color": "red"}}>
+ */
