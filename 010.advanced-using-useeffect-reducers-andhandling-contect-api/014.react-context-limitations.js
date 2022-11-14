55e10b17c0572791acd70b6c9f10089fe76ed81d
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-11-14 17:48:24 +0530

    014.react-context-limitations.js

---
+++ 010.advanced-using-useeffect-reducers-andhandling-contect-api/014.react-context-limitations.js
@@ -0,0 +1,13 @@
+/*
+
+    React context limitations
+
+    There are some limitations to react context hook:
+
+        1. React context is not optimised for high frequency changes in which state changes are happening ever
+        millisecond.
+
+        2. React context also shouldn't be used to replace all component communications and props as props are vital for
+        component configuration.
+
+ */
