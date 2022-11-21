e66259aed5d6f8b1b0d7c173f8bf644d8fbcd346
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-11-21 09:47:03 +0530

    001.what-and-why.js

---
+++ 013.an-alternative-way-of-building-components-class-based-components/001.what-and-why.js
@@ -0,0 +1,14 @@
+/*
+
+    What and why
+
+    Class-Based Components exist and they are an alternative to functions.
+    Classes are not a React feature, classes exist in modern JavaScript. We can define a class with a render method and
+    the word render is a reserved keyword.
+
+    Prior to 16.8 version we had to use only class based components as state couldn't be changed using functional
+    components and we couldn't also manage side-effects in functional components. In the version 16.8 react introduced
+    the concept of hooks. These are functions that bring features to functional components.
+    Class based components cannot use react hook.
+
+ */
