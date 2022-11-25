6c6b989dd5ca860542e6ea837f09f4d5859621cc
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-11-25 09:54:19 +0530

    002.our-starting-app-and-backend.js

---
+++ 014.sending-http-requests-eg-connecting-to-a-database/002.our-starting-app-and-backend.js
@@ -0,0 +1,17 @@
+/*
+
+    Starting App & Backend
+
+    API stands for Application Programming Interface.
+
+    HTTP requests means that the code we're dealing with has a clearly defined interface, on how we can achieve certain
+    results and do certain tasks, and when we talk about APIs in the context of HTTP requests.
+
+    There are 2 different standards for how a server should expose its data:
+        1. REST
+        2. GraphQL
+
+    Different URLs to which you send different requests will give you different chunks of data. That's what makes it an
+    API.
+
+ */
