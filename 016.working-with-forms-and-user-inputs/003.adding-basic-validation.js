3f229f7f83268d7b39b3625ee33e6dbbd6883662
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-12-06 11:26:14 +0530

    003.adding-basic-validation.js

---
+++ 016.working-with-forms-and-user-inputs/003.adding-basic-validation.js
@@ -0,0 +1,14 @@
+/*
+
+    Adding basic validation
+
+    It is a good practise to not just validate inputs on the client side but also on the server as the users can
+    manipulate with the code from the client side through console window.
+    Client side validation is just good for providing users with feedback and not for security mechanism.
+
+    One important validation check is not allowing the user to submit, empty input or having many empty spaces in
+    the input. This can be done by using the javascript function trim().
+
+    It is a good user experience to notify the user that the form is not being submitted as the input is invalid.
+
+ */
