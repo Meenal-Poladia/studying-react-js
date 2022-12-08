fb0eb15bab81a0d9600528ce7232e15407d7d221
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-12-08 10:53:50 +0530

    002.redux-versus-react-context.js

---
+++ 018-diving-into-redux-alternative-to-the-context-API/002.redux-versus-react-context.js
@@ -0,0 +1,13 @@
+/*
+
+    Redux versus React Context
+
+    Disadvantages of React Context:
+        1. React Context may have a very complex setup and managing state with react context can become quite
+           complex.
+        2. When building a large application with a lot of components and a lot of things going on,
+           using React Context can end up with spaghetti code and deeply nested JSX code.
+        3. Performance - If the data changes a lot then context is not a good option, it can only be helpful for
+           low frequency tasks like changing theme, user authentication.
+
+ */
