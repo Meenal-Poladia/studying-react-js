656ab3a1b200f4e2652fdb3b05db15d7845cf07d
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-12-12 17:57:27 +0530

    006.creating-a-redux-store.js

---
+++ 018-diving-into-redux-alternative-to-the-context-API/006.creating-a-redux-store.js
@@ -0,0 +1,16 @@
+/*
+
+    Creating A Redux Store
+
+    To create a store to store the state, we have to first install redux in our project using npm.
+    Later import createStore function from redux. This function accepts a reducer as an argument. We have to export
+    the index.js of store as it is only then that the data will be available to other components in the project.
+
+    example
+    store -> index.js
+
+    const store = redux.createStore(reducerFunction);
+
+    export default store;
+
+ */
