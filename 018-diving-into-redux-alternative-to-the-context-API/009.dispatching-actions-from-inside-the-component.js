3c69d14fd0198000fa948cf1e98722237d179b55
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-12-16 09:59:25 +0530

    009.dispatching-actions-from-inside-the-component.js

---
+++ 018-diving-into-redux-alternative-to-the-context-API/009.dispatching-actions-from-inside-the-component.js
@@ -0,0 +1,13 @@
+/*
+
+    Dispatching Actions From Inside The Component
+
+    useDispatch returns a function which we store in a variable. We use the dispatch function to send data to the store.
+
+    Dispatching an action is an object which takes the type property which is one of the identifier of the reducer
+    specified in the reducer function.
+
+    example -
+    const dispatch = useDispatch();
+
+*/
