ea3b0f98d560eca413dd889b1b0ae0f74bad044a
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-01-19 18:05:55 +0530

    004.finalising-the-fetching-logic.js

---
+++ 019.advanced-redux/004.finalising-the-fetching-logic.js
@@ -0,0 +1,19 @@
+/*
+
+    Exploring the Redux devtool
+
+    Redux devtools are extra tools which make debugging Redux and our Redux state a bit easier, because in more complex
+    applications with a lot of Redux state, handling a lot of different slices and different actions can be
+    difficult.
+
+    We can simply search for Redux Devtools and we'll find a GitHub repository about the Redux Devtools. It can be
+    installed as a browser extension. The extension cannot be operated from incognito window.
+
+    The init in the console represents the initial state of the app. Whenever any action is dispatched it looks in
+    the console window. We can also see the difference in the current state and the state once the action is dispatched.
+    This is very useful when trying to understand the impact of actions.
+
+    There is also a feature of time travelling in which we can click on the action dispatched and revisit the state
+    to debug various states.
+
+ */
