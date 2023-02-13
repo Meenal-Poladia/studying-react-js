ca2d89f52e4cea974dcdbcff235809a76165659b
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-02-13 15:56:18 -0500

    002.installing-react-router.js

---
+++ 020.building-a-multi-page-SPA-with-react-router/002.installing-react-router.js
@@ -0,0 +1,15 @@
+/*
+
+    Installing React Router
+
+    There is a very popular package that helps us with creating multi-page React applications, and that package is
+    called React Router, which is involved in client-side routing.
+
+    Routing simply means that different URLs and different paths in the URL load different pages.
+
+    React-Router-DOM is the React Router version for React apps that run in the browser. We can download it by using
+    the command -> npm install react-router-dom
+
+    To download a specific react router version use the command -> npm install react-router-dom@5
+
+*/
