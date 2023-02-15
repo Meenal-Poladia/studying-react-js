258ba90f8b5e2adf287b0d64ce78ad4a94a8c677
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-02-15 10:18:31 -0500

    006.adding-dynamic-routes-with-params.js

---
+++ 020.building-a-multi-page-SPA-with-react-router/006.adding-dynamic-routes-with-params.js
@@ -0,0 +1,12 @@
+/*
+
+    Adding Dynamic Routes With Params
+
+    When we have many items and we wish to show detailed data about each item when clicked on it, we use
+    dynamic routes. In this we navigate to a new screen showing details about the product clicked.
+
+    To generate dynamic path we use the syntax -> "/product/:id".
+    This means that react knows that our domain will be products but everything defined after : can take any value,
+    number and is completely dynamic.
+
+ */
