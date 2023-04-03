aa355b5213d8f7e9bc79ad8db726cc2edeb75ea1
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-03 16:02:56 -0400

    005.navigating-between-pages-with-links.js

--- 020B.revised-building-multipage-SPA-with-react-router/005.navigating-between-pages-with-links.js
+++ 020B.revised-building-multipage-SPA-with-react-router/005.navigating-between-pages-with-links.js
@@ -2,6 +2,28 @@
 
     Navigating between pages with links
 
+    When we are using the anchor tag for the user to navigate to the new route we are sending an additional request
+    due to which the page gets refreshed again. Refreshing the page might lead to losing the current state, network
+    request and loading a new html page.
 
+    The Link element works like an anchor tag which navigates the user to another link without making any API call or
+    rendering an HTML page.
+
+    example - 1
+    import React from 'react';
+    import {Link} from "react-router-dom";
+
+    const Home = () => {
+        return (
+            <div>
+                <h3>HomePage</h3>
+                <p>Go to
+                    <Link to="/products"> the list of Products</Link>
+                </p>
+            </div>
+        );
+    };
+
+    export default Home;
 
  */
