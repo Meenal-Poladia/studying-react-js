ca31ceaf4ae49f2e1f0e7c7d4f19dde7f45c2052
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-04 15:58:19 -0400

    009.navigating-programmatically.js

---
+++ 020B.revised-building-multipage-SPA-with-react-router/009.navigating-programmatically.js
@@ -0,0 +1,21 @@
+/*
+
+    Navigating Programmatically
+
+    Except for using the Navlink or Link to programmatically navigate users to another page. There is a hook called
+    as the useNavigate which navigates user to another page.
+
+    example
+    const navigate = useNavigate();
+
+    function navigateHandler = () => {
+        navigate("/products");
+    }
+
+    return <>
+        <button>
+            Click me to Navigate
+        </button>
+    </>
+
+ */
