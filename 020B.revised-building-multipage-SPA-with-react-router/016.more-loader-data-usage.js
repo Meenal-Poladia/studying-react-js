159a52d89157cc8216f931b0a3773df935715739
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-06 11:45:24 -0400

    016.more-loader-data-usage.js

---
+++ 020B.revised-building-multipage-SPA-with-react-router/016.more-loader-data-usage.js
@@ -0,0 +1,9 @@
+/*
+
+    More Loader Data usage
+
+    We cannot access the data from the http request of the parent routes. We get data on the routes defined at the
+    lower level. useLoaderData() can be used in the element that's assigned to a route and in all the components
+    that might be used inside that element.
+
+ */
