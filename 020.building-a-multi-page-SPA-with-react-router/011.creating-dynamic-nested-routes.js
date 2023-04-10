de44dc18380836bf51d9ec246c857df161965a68
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-10 09:22:22 -0400

    011.creating-dynamic-nested-routes.js

---
+++ 020.building-a-multi-page-SPA-with-react-router/011.creating-dynamic-nested-routes.js
@@ -0,0 +1,15 @@
+/*
+
+    Creating Dynamic Nested Routes
+
+    It is possible to create a dynamic nested route inside an existing route and it is also important that the path
+    needs to be registered where the component is.
+
+    For eg:
+    <Route to:`quotes/${params.id}/comments`></Route>
+
+    OR
+
+    <Route to:`quotes/:quotesId/comments`></Route>
+
+ */
