289a83682abe1dc3cabc00b3902866064fc0e3e5
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-15 08:30:18 -0400

    021.project-setup-and-installing-react-router-dom.js

---
+++ 020.building-a-multi-page-SPA-with-react-router/021.project-setup-and-installing-react-router-dom.js
@@ -0,0 +1,17 @@
+/*
+
+    Project setup and installing react router dom
+
+    The routing functionality of watching the URL and loading different content is not build-in react. To support these
+    URL changes and different use cases we install a package called react-router-dom.
+
+    The first step is, we must define the routes we want to support, so we must define which URLs, which paths we
+    want to support and which components should be loaded for different paths.
+
+    The second step is to activate our router and load the route definitions that we defined in the first step.
+
+    The third step, is to make sure that we have all these components that we want to load and that they may
+    also provide some means of navigating between those pages so that our users can move smoothly between the
+    different pages.
+
+ */
