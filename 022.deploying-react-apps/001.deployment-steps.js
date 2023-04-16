55ed012a744268be2aa012549effa957a30820bc
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-16 10:40:41 -0400

    001.deployment-steps.js

---
+++ 022.deploying-react-apps/001.deployment-steps.js
@@ -0,0 +1,18 @@
+/*
+
+    Deployment Steps
+
+    After writing the code and testing it thoroughly it is important to optimise the code.
+
+    Once the code is optimised it is important to build the app for production. Writing a script will help to output
+    a production ready bundle or our code which is unified and automatically optimised to be as small as possible.
+    We can take this output and move to the server. Shipping less code helps to load the website faster.
+
+    Once the code is build we take that bundle and upload it on the server of our choice. The last step is to configure
+    the server or our hosting provider offering.
+
+    Flow chart of Deployment Steps
+
+    Test code -> Optimise code -> Build app for Production -> Upload Production code to Server -> Configure Server
+
+ */
