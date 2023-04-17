b8ba5d2ec0f251a779b2643f7db81ef4919e227c
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-17 18:29:02 -0400

    004.deployment-example.js

---
+++ 022.deploying-react-apps/004.deployment-example.js
@@ -0,0 +1,20 @@
+/*
+
+    Deployment Example
+
+    React SPA is a static website which contains only HTML, CSS and Javascript files with a couple of images. It does
+    not contain any code that must be executed on the browser. All the code will be parsed by the browser and will be
+    executed on the computers of our visitors.
+    Therefore, we only need a Static Site Host. We don't need any hosting provider that executes code on the server.
+
+    To deploy app on the firebase we need a google account. Once we login we need to create a new project. If we
+    want to add google analytics then we accept the test. Once the setup is completed. On the side bar we click
+    on the build option and later choose Hosting.
+
+    Firebase will walk us through how to deploy apps. We need to install firebase CLI on our project to use its command.
+    Follow the steps which are shown on the modal. After initialing firebase settings on our local computer we deploy
+    the app.
+
+    To take the website offline once it is on the cloud then we use the command -> firebase hosting:disable
+
+ */
