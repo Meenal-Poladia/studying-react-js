4303062ed7be18b5509f14ed4ad7945c8de27eee
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-17 18:27:40 -0400

    003.building-the-code-for-production.js

---
+++ 022.deploying-react-apps/003.building-the-code-for-production.js
@@ -0,0 +1,12 @@
+/*
+
+    Building the code for Production
+
+    When we use the command npm run build, under the hood this will produce a code bundle with highly optimized and
+    transformed code which is ready to be uploaded. Once the build process is completed it creates a build folder which
+    contains the code that needs to be deployed on the server.
+    In the static folder we have our optimised javascript. Its not a readable code but it is valid and executable.
+
+    It is the content of the build folder which needs to be uploaded.
+
+ */
