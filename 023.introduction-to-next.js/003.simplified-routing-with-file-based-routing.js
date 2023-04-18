014dba434b132cd4084184ea485af1317ab86e54
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-18 11:48:54 -0400

    003.simplified-routing-with-file-based-routing.js

---
+++ 023.introduction-to-next.js/003.simplified-routing-with-file-based-routing.js
@@ -0,0 +1,11 @@
+/*
+
+    Simplified Routing with file based routing
+
+    In react, we change the content viewed according to the url and thus we do not refresh the page and so
+    it is a one page application. We do not make various request calls for each url.
+
+    In Next.js we define routes using files and folders. We have a special folder for NextJS apps which has
+    to be named as pages and then we are structuring that folder, defining the routes and paths your page supports.
+
+ */
