0e9637eb601740c47a323d36be6c9708d2153d1d
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-18 11:49:29 -0400

    004.building-fullstack-apps.js

---
+++ 023.introduction-to-next.js/004.building-fullstack-apps.js
@@ -0,0 +1,10 @@
+/*
+
+    Building Fullstack Apps
+
+    With NextJS it's very easy to add our own backend API into our react project using NodeJS code.
+
+    That allows us to add code for storing data to a database or to files, getting data from there, adding
+    authentication and all off that.
+
+ */
