48cf07717ac848c4fb503c5b6f19cb199efbd62f
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-19 15:52:06 -0400

    006.adding-first-page.js

---
+++ 023.introduction-to-next.js/006.adding-first-page.js
@@ -0,0 +1,22 @@
+/*
+
+    Adding First Page
+
+    We can delete the api folder in the pages directory and home.modules.css in the styles folder.
+
+    In the pages folder we have the index.js file which holds code which we want to render when the app loads.
+    There is no html file, index.js file does the work of html.
+    Pages with /domain name read the page which is made in the pages folder.
+    The name of the file in the pages folder is the domain name which opens when the request is hit.
+
+    example - our.domain.com/news[file name inside pages]
+
+    React component which needs to be loaded goes into the file.
+
+    To run the code we need to use the command npm run dev.
+
+    When we right click on the page and choose view source page then we can see the actual source code of the page.
+    This code is sent back by the server. As this page is sent from the server, we do not have any initial flickering
+    of the page. This content can be viewed by our users as well as the search engines.
+
+ */
