a5a52c621a0e142d614b5ef868fd1e8cd7e23b05
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-19 15:50:24 -0400

    005.creating-a-new-next-js-project.js

---
+++ 023.introduction-to-next.js/005.creating-a-new-next-js-project.js
@@ -0,0 +1,20 @@
+/*
+
+    Creating a new Next js Project
+
+    We need to have node js installed on our pc to install the nextjs library.
+    Go to the terminal and use the command npx create-next-app. We will be prompt to confirm and give a project name.
+
+    There are 3 important directories which the new next.js project consist of:
+        1. pages
+        2. styles
+        3. public - this folder consist of all the images which we will use as resources in our project.
+
+    In next js project we do not have a index.html because nextjs has its build in pre-rendering. This single page
+    application is dynamically pre-rendered when a request reaches the data server so that we can return an initial
+    page with content.
+
+    The pages folder will be the most important folder because that is where we will set up the file based routing
+    for our application.
+
+ */
